f6637ec6a26555dac80070e8681f3d778f04069d
Al Tsaplin <al.tsaplin@gmail.com>
2024-05-17 12:50:59 +0300

    add(homework) For lesson #02

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 'use strict';
+/* 
+1) Следующим переменным присвоить значения
 
-let title,
-	screens,
-	screenPrice,
-	rollback,
-	fullPrice,
-	adaptive;
+	title- строка с названием проекта,
+	screens - строка с названиями типов экранов через запятую ("Простые, Сложные, Интерактивные"),
+	screenPrice- любое число,
+	rollback - любое число от 1 до 100,
+	fullPrice- любое число (сколько хотите заработать),
+	adaptive- булево значение
+*/
+const title = 'Glo Javascript Course Project',
+	screens = 'Простые, Сложные, Интерактивные',
+	screenPrice = 2000,
+	rollback = 5,
+	fullPrice = 50000,
+	adaptive = true;
 
-alert('Hello world!');
-console.log('Hello world!');
+/* 	
+2) Используя методы и свойства:
+
+	Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+	Вывести в консоль длину строки из переменной screens
+	Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+	Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+	Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100)) 
+*/
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log('“Стоимость вёрстки экрана: ' + screenPrice + ' рублей.”\n' + '“Стоимость разработки сайта: ' + fullPrice + ' рублей.”');
+
+const screensVariety = screens.toLowerCase().split(', ');
+
+console.log(screensVariety);
+
+const rollbackCount = fullPrice * (rollback / 100);
+
+console.log(rollbackCount);
