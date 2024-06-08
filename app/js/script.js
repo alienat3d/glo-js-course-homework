c002dfcd13efaa34cebc1117705589e660bd5b4c
Al Tsaplin <al.tsaplin@gmail.com>
2024-06-08 12:20:04 +0300

    wip(homework) For lesson #09 (undone '.total-input')

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,34 @@
 'use strict';
 /* 
-1) Сделать проверку при получении данных:
-- ответ на вопрос "Как называется ваш проект?" - строка
-- ответ на вопрос "Какие типы экранов нужно разработать?" - строка
-- ответ на вопрос "Сколько будет стоить данная работа?" - число
-- ответ на вопрос "Какой дополнительный тип услуги нужен?" - строка
-- ответ на вопрос "Сколько это будет стоить?" - число
-Что значит проверка данных: где должен быть текст там только текст(голые цифры не должно пропускать, а текст с цифрами - должно. Пример: "Купил ВАЗ 2108" - ок, "4567989" - нет), где цифры только цифры!
-Если проверку не прошло, то переспрашивать
+Необходимо выполнить данное задание в скрипт-файле из прошлых уроков (перенести его в проект из файлов для урока, заменить уже имеющийся пустой script.js)
+Не забывайте, все элементы со страницы мы получаем в самом верху кода. (в самую первую очередь)
+1) Задание по проекту, получить каждый элемент в отдельную переменную:
+=>> Получить заголовок "Калькулятор верстки" через метод getElementsByTagName. (тэг h1, получить именно элемент, а не коллекцию)
+=>> Получить кнопки "Рассчитать" и "Сброс" через метод getElementsByClassName. (класс handler_btn)
+=>> Получить кнопку "+" под выпадающим списком через метод querySelector. (класс screen-btn)
+=>> Получить все элементы с классом other-items в две разные переменные. В первую элементы у которых так же присутствует класс percent, во вторую элементы у которых так же присутствует класс number через метод querySelectorAll.
+=>> Получить input type=range через его родителя с классом rollback одним запросом через метод querySelector.
+=>> Получить span с классом range-value через его родителя с классом rollback одним запросом через метод querySelector.
+=>> Получить все инпуты с классом total-input справа через метод getElementsByClassName. (класс total-input, получить именно элементы, а не коллекции)
+=>> Получить все блоки с классом screen в изменяемую переменную ( let ) через метод querySelectorAll (далее мы будем переопределять ее значение)
 */
-const appData = {
+
+const title = document.getElementsByTagName('h1')[0];
+const calcResetBtns = document.getElementsByClassName('handler_btn');
+const plusBtn = document.querySelector('.screen-btn');
+const optionPercentCheckboxes = document.querySelectorAll('.other-items.percent');
+const optionNumCheckboxes = document.querySelectorAll('.other-items.number');
+const rollbackController = document.querySelector('.rollback span.range-value');
+const priceInputsCollection = document.getElementsByClassName('input.total-input');
+let priceInputs = [];
+
+for (let index = 0; index < priceInputsCollection.length; index++) {
+	priceInputs.push(priceInputsCollection[index]);
+}
+
+console.log(priceInputs);
+
+/* const appData = {
 	ROLLBACK: 5,
 	title: '',
 	screens: [],
@@ -100,6 +119,6 @@ const appData = {
 		console.log(appData.servicePercentPrice);
 		console.log(appData.screens);
 	}
-}
+} */
 
-appData.start();
+// appData.start();
