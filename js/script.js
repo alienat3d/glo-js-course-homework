f0695f17de70e9524da45a0aa45da741470fa398
Al Tsaplin <al.tsaplin@gmail.com>
2024-06-13 18:09:17 +0300

    wip(homework) For lesson#13: (still in work)

--- js/script.js
+++ js/script.js
@@ -7,24 +7,32 @@ const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-const toDoData = [];
+let toDoData = [];
+let data = [];
 
 const render = function () {
+
 	todoList.innerHTML = '';
 	todoCompleted.innerHTML = '';
+
+	for (let index = 0; index < localStorage.length; index++) {
+		toDoData.push(JSON.parse(localStorage.getItem('note #' + index)));
+	}
+
+	console.log(toDoData);
 
 	toDoData.forEach(function (item) {
 		const li = document.createElement('li');
 
 		li.classList.add('todo-item');
-
-		li.innerHTML = '<span class="text-todo">' +
-			item.text +
-			'</span><div class="todo-buttons">' +
-			'<button class="todo-remove">' +
-			'</button><button class="todo-complete">' +
-			'</button></div>';
-
+		li.dataset.num = item.id;
+		li.innerHTML = `
+			<span class="text-todo">${item.text}</span>
+			<div class="todo-buttons">
+				<button class="todo-remove"></button>
+				<button class="todo-complete"></button>
+			</div>
+		`
 		if (item.completed) {
 			todoCompleted.append(li);
 		} else {
@@ -36,22 +44,40 @@ const render = function () {
 				item.completed = !item.completed;
 				render();
 			});
+
+		li.querySelector('.todo-remove')
+			.addEventListener('click', function (evt) {
+				localStorage.removeItem('note #' + evt.target.closest('li').dataset.num);
+				render();
+			});
 	});
 }
 
 todoControl.addEventListener('submit', function (evt) {
 	evt.preventDefault();
 
-	const newToDo = {
-		text: headerInput.value,
-		completed: false
+	if (headerInput.value) {
+		const newToDo = {
+			id: 0,
+			text: headerInput.value,
+			completed: false
+		}
+		if (toDoData.length > 0) {
+			const idPlusOne = toDoData[toDoData.length - 1].id + 1;
+			newToDo.id = idPlusOne;
+		}
+		toDoData.push(newToDo);
+
+		localStorage.clear();
+
+		toDoData.forEach(function (obj, idx) {
+			localStorage.setItem('note #' + idx, JSON.stringify(obj));
+		});
+
+		headerInput.value = '';
+
+		render();
 	}
-
-	toDoData.push(newToDo);
-
-	headerInput.value = '';
-
-	render();
 })
 
 render();
